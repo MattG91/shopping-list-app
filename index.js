abf181931fe8a1c10c099ad56153a59e6ceddd94
MattG91 <48453914+MattG91@users.noreply.github.com>
2020-07-29 05:16:02 -0600

    added notes on what function do

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// function for adding new items to the list with "add item" button
 $(function() {
   $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
     const newListItem = $('#shopping-list-entry').val();
-    $('#shopping-list-entry').val(' ');
+    $('#shopping-list-entry').val('');
     $('.shopping-list').append(`
       <li>
         <span class="shopping-item">${newListItem}</span>
@@ -19,10 +20,12 @@ $(function() {
     });
 });
 
+// Add line-through to item with "check" buttom
 $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
    $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
  });
 
- $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
+// Delete item with "delete" button
+$(".shopping-list").on("click", ".shopping-item-delete", function(event) {
   $(this).closest("li").remove();
 });
